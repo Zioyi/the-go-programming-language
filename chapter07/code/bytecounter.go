acdaa0700c19b958b001cca25f0d5e16b132199e
Zioyi <yafei.zhao@shanbay.com>
2021-05-30 23:29:13 +0800

    init chapter07

---
+++ chapter07/code/bytecounter.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+type ByteCounter int
+
+func (c *ByteCounter) Write(p []byte) (int, error) {
+	*c += ByteCounter(len(p))
+	return len(p), nil
+}
+
+func byteCounterExample() {
+	var c ByteCounter
+	var name = "Dolly"
+	fmt.Fprintf(&c, "hello, %s", name)
+	fmt.Println(c)
+}
